5603ca19c6a3e6e127e0b2d4af3df06da8351521
Dolly <131373640+Dolly62@users.noreply.github.com>
2023-07-13 00:02:41 +0530

    First Component
    Starting point

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Added one component */}
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
