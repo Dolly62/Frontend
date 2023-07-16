d2338b1789442bdb893daef6c38856ab6e048a91
Dolly <131373640+Dolly62@users.noreply.github.com>
2023-07-17 00:14:18 +0530

    Delete Button Added

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,15 +1,33 @@
+import React, {useRef} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+// import Card from './Card';
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const expenseItemRef = useRef(null);
+  
+  const deleteExpense = () => {
+if (expenseItemRef.current){
+  expenseItemRef.current.remove();
+}
+  
+  };
   // First component
   return (
-    <div className="expense-item">
+    <div className="expense-item" ref={expenseItemRef}>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title = {props.title} location={props.location} amount={props.amount}/>
+      <ExpenseDetails
+        title={props.title}
+        location={props.location}
+        amount={props.amount}
+      />
+      <button className="btn" onClick={deleteExpense}>
+        Delete Expense
+      </button>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
